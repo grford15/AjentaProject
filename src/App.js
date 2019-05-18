048f22f3e62cf6dc5bfc213d67ddd531a91c99b4
Greg <g.rutherford10@gmail.com>
2019-05-18 18:21:04 +0100

    add functionality to return meetings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
+import axios from 'axios';
 import NavBar from './components/nav-bar';
 import SideNavBar from './components/side-nav-bar';
 import Home from "./components/Home";
 import Schedule from "./components/Schedule";
 
-function App() {
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+        meetings: undefined,
+        users: undefined
+    }
+}
+
+async componentDidMount(){
+    const { data: meetings } = await axios.get('https://cors-anywhere.herokuapp.com/https://coding-test.ajenta.io/meetings');
+    const { data: users } = await axios.get('https://cors-anywhere.herokuapp.com/https://coding-test.ajenta.io/users');
+    this.setState({ meetings, users })
+    
+}
+
+render(){
   return (
     <div>
       <header>
@@ -16,13 +34,14 @@ function App() {
         <div className="main-container">
         <Router>
           <SideNavBar />
-            <Route exact path="/" component={Home} />
+            <Route exact path="/" render={(props) => <Home {...props} meetings={this.state.meetings} users={this.state.users} />} />
             <Route path="/schedule" component={Schedule} />
         </Router>
         </div>
       </main>
     </div>
   );
+}
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,13 @@
 import React from 'react';
-
 import "../styling/Home.css";
 
-const Home = () => 
+class Home extends React.Component {
+
+
+    render() {
+        
+    return(
+        this.props.meetings !== undefined && this.props.users !== undefined &&
     <div className="component-container">
         <h3 className="page-title">Scheduled Meetings</h3>
         <div className="div-table">
@@ -13,14 +18,26 @@ const Home = () =>
                 <div className="div-table-col">Date</div>
                 <div className="div-table-col">Start</div>
             </div>
-            <div className="div-table-row">
-                <div className="div-table-col">20202</div>
-                <div className="div-table-col">Greg</div>
-                <div className="div-table-col">Greg Rutherford</div>
-                <div className="div-table-col">03/05/2019</div>
-                <div className="div-table-col">15:30</div>
+            {
+                this.props.meetings.map((meeting, index) => {
+                    let startDate = meeting.start_time.slice(0, 10);
+                    let startTime = meeting.start_time.slice(11, 19);
+                return(
+            <div className="div-table-row" key={index}>
+                <div className="div-table-col">{meeting.callid}</div>
+                <div className="div-table-col">{meeting.owner}</div>
+                <div className="div-table-col">{meeting.name}</div>
+                <div className="div-table-col">{startDate}</div>
+                <div className="div-table-col">{startTime}</div>
             </div>
-        </div>
+                )})
+        }
+        
     </div>
+        
+    </div>
+    )
+    }
+}
 
 export default Home 
