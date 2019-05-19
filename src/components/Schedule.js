d3dbd5ca605b2602d748aff60700893da1730f33
Greg <g.rutherford10@gmail.com>
2019-05-19 17:50:39 +0100

    add functionality to save inputs to state

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -7,19 +7,20 @@ class Schedule extends React.Component {
     constructor(props){
         super(props)
             this.state = {
-                title: '',
+                name: '',
                 description: '',
                 duration: '',
                 guests: []
             }
         
-        this.handleChange = this.handleChange.bind(this);
+        this.handleGuestChange = this.handleGuestChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    handleChange(e){
+    handleGuestChange(e){
 
-        let newGuest = e.target.value;
+        let newGuest = parseInt(e.target.value);
     
         this.setState(state => {
             const guests = state.guests.concat(newGuest);
@@ -31,6 +32,12 @@ class Schedule extends React.Component {
 
     handleClick(){
         console.log("Clicked!");
+    }
+
+    handleChange(e){
+        this.setState( {[e.target.name] : e.target.value})
+        console.log(this.state);
+        
     }
         
     
@@ -45,15 +52,15 @@ class Schedule extends React.Component {
             <form className="single-form">
                 <div className="form-group">
                     <label>Title</label>
-                    <input type="text" className="form-control" id="meeting-title" placeholder="Enter a Title" />
+                    <input type="text" name="name" className="form-control" id="meeting-title" placeholder="Enter a Title" onChange={this.handleChange}/>
                 </div>
                 <div className="form-group">
                     <label>Description</label>
-                    <input type="text" className="form-control" id="meeting-description" placeholder="Enter a Description" />
+                    <input type="text" name="description" className="form-control" id="meeting-description" placeholder="Enter a Description" onChange={this.handleChange} />
                 </div>
                 <div className="form-group">
                     <label>Duration</label>
-                    <select className="form=control" id="meeting-duration">
+                    <select name="duration" className="form=control" id="meeting-duration" onChange={this.handleChange}>
                         <option>1h</option>
                         <option>2h</option>
                         <option>3h</option>
@@ -64,19 +71,21 @@ class Schedule extends React.Component {
             <form className="single-form">
                 <div className="form-group">
                     <label>Add Guests</label>
-                    <select className="form-control" id="user-selector" onChange={this.handleChange}>
+                    <select className="form-control" id="user-selector" onChange={this.handleGuestChange}>
                         {this.props.users.map((user, index) => {
                             let userName = user.first_name + " " + user.last_name;
                             return(
-                                <option key={index} value={userName}>{userName}</option>
+                                <option key={index} value={user.id}>{userName}</option>
                             )
                         })}
                     </select>
                 </div>
                 {
                     this.state.guests.map((user, index) => {
+                    let guest = this.props.users.find(guest => guest.id === user);
+                    let guestName = guest.first_name + " " + guest.last_name;
                     return(
-                        <li key={index}>{user}</li>
+                        <li key={index}>{guestName}</li>
                     )
                 } )}
             </form>
