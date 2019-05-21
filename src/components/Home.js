bd6476967220b39998225b08584329274fde5b30
Greg <g.rutherford10@gmail.com>
2019-05-21 19:35:14 +0100

    change styling on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ class Home extends React.Component {
     return(
         this.props.meetings !== undefined && this.props.users !== undefined &&
     <div className="component-container">
-        <h3 className="page-title">Scheduled Meetings</h3>
+        <h3 className="page-title">Scheduled Meetings ({this.props.meetings.length})</h3>
         <div className="div-table">
             <div className="div-table-row-header">
                 <div className="div-table-col">Call Id</div>
@@ -20,7 +20,7 @@ class Home extends React.Component {
             </div>
             {
                 this.props.meetings.map((meeting, index) => {
-                    let startDate = meeting.start_time.slice(0, 10);
+                    let startDate = new Date(meeting.start_time.slice(0, 10));
                     let startTime = meeting.start_time.slice(11, 19);
                     let theOwner = this.props.users.find(user => user.id === meeting.owner)
                 return(
@@ -28,7 +28,7 @@ class Home extends React.Component {
                 <div className="div-table-col">{meeting.callid}</div>
                 <div className="div-table-col">{theOwner.first_name + " " + theOwner.last_name}</div>
                 <div className="div-table-col">{meeting.name}</div>
-                <div className="div-table-col">{startDate}</div>
+                <div className="div-table-col">{startDate.toDateString()}</div>
                 <div className="div-table-col">{startTime}</div>
             </div>
                 )})
--- src/components/side-nav-bar.js
+++ src/components/side-nav-bar.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import home from '../public/assets/home-btn.png';
+import schedule from '../public/assets/schedule-btn.png';
 
 import "../styling/side-nav-bar.css"
 
@@ -7,9 +9,11 @@ const SideNavBar = () =>
     <div id="side-bar">
         <div className="links">
             <a href="/">Home</a>
+            {/* <img src={home} alt="home button" /> */}
         </div>
         <div className="links">
             <a href="/schedule">Schedule</a>
+            {/* <img src={schedule} alt="schedule button" /> */}
         </div>
     </div>
 
